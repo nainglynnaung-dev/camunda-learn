a61e30b150d73165d2c821dc9757ebc0299575a7
Naing Lynn Aung <133348339+nainglynnaung-dev@users.noreply.github.com>
2026-02-04 17:27:12 +0630

    error handling for bpmn

--- first/src/main/java/com/ly/workflow/ReserveSeatOnBoat.java
+++ first/src/main/java/com/ly/workflow/ReserveSeatOnBoat.java
@@ -1,6 +1,7 @@
 package com.ly.workflow;
 
 import jakarta.inject.Named;
+import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
@@ -17,8 +18,12 @@ public class ReserveSeatOnBoat implements JavaDelegate {
         double doubleMoney=Double.parseDouble(money);
         if (doubleMoney >=10000.00){
             ticketType="First Class";
-        } else if (doubleMoney <=5000.00) {
+        } else if (doubleMoney >=5000.00) {
             ticketType="Business Class";
+        } else if (doubleMoney<=10) {
+            ticketType="StowAway Class";
+            throw new BpmnError("FALL_Overboard","A cheap ticket has lead to a small wave throwing you overboard");
+
         }
         execution.setVariable("ticketType",ticketType);
 
